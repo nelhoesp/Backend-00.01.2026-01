d0e64c7c772f5601f650bf8e02183e341937b75a
nelhoesp <nelhofabrizio@gmail.com>
2026-03-22 16:29:00 -0500

    feat(cors): update allowed methods to include PATCH in CORS configuration

--- Hackaton09/sv72672266/src/server.js
+++ Hackaton09/sv72672266/src/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
     next();
 });
 
