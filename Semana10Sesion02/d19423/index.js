c5d809f805265c9da32a54b0d4df1525d920f391
Roberto Pineda <rpineda@x-codec.net>
2026-03-26 22:52:26 -0500

    Hackaton 10

--- Semana10Sesion02/d19423/index.js
+++ Semana10Sesion02/d19423/index.js
@@ -1,6 +1,18 @@
 console.log("Inicio de la aplicacion");
 require("dotenv").config();
-const { MongoClient } = require('mongodb');
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const {alumnoRouter} = require('./alumno.router')
 
-const uri = process.env.MONGO_URI;
-const client = new MongoClient(uri);
+const app = express();
+
+app.use(express.json());
+app.use('/alumnos',alumnoRouter)
+
+app.get('/',(req,res)=>{
+    res.send({message: "online"})
+})
+
+app.listen(PORT,()=>{
+    console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
